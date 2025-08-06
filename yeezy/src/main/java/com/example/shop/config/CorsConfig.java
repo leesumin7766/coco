90d7daf12f308a2dd482c33e55e61ec2003ee394
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-08-07 05:44:56 +0900

    DB 수정, EC2까지 마무리 GitAction 자동화 해야함

--- yeezy/src/main/java/com/example/shop/config/CorsConfig.java
+++ yeezy/src/main/java/com/example/shop/config/CorsConfig.java
@@ -7,13 +7,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000"); // 정확한 origin 명시
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://3.38.108.76:3000",
+                "http://3.38.108.76:80",
+                "http://3.38.108.76:22"
+        )); // 정확한 origin 명시
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true); // Authorization 허용
