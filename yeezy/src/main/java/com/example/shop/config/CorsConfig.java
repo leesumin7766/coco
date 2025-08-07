5c2a1f0434693f27a5b438a7c1dcd8eadf170ac1
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-08-08 04:29:25 +0900

    DB 수정, EC2까지 마무리 GitAction 자동화 해야함

--- yeezy/src/main/java/com/example/shop/config/CorsConfig.java
+++ yeezy/src/main/java/com/example/shop/config/CorsConfig.java
@@ -6,28 +6,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://3.38.108.76:3000",
-                "http://3.38.108.76:80",
-                "http://3.38.108.76:22"
-        )); // 정확한 origin 명시
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.setAllowCredentials(true); // Authorization 허용
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")                       // 모든 경로에 대해
+                .allowedOrigins(
+                        "http://localhost:3000",            // 로컬 개발
+                        "http://3.38.108.76:3000",
+                        "http://3.38.108.76:80",
+                        "http://3.38.108.76:22"// 실제 배포된 프론트
+                )
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
-
