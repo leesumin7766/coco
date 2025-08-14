f38984399f162811a43dca50cdf369916138ba5a
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-08-15 07:01:46 +0900

    CorsConfig에서 실제 오리진(3.37.52.16) 추가 SecurityConfig에 OPTIONS /** permitAll() 추가

--- yeezy/src/main/java/com/example/shop/config/CorsConfig.java
+++ yeezy/src/main/java/com/example/shop/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**")                       // 모든 경로에 대해
                 .allowedOrigins(
-                        "http://localhost:3000",            // 로컬 개발
+                        "http://localhost:*",           // 로컬 개발
 //                        "http://3.38.108.76:3000",
 //                        "http://3.38.108.76:80",
 //                        "http://3.38.108.76:22", // 실제 배포된 프론트
