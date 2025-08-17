94169ababccde5c6ae6d5fcb2b02341ff2cfc773
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-08-17 21:27:18 +0900

    loaclhost경로  삭제

--- shop-react/src/pages/LoginPage.jsx
+++ shop-react/src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8080/api/auth/login', {
+      const response = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
